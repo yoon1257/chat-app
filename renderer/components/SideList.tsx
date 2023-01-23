e12ac3dd12283b0b86ceb8ba94ddc7b0565cd68b
yuna <yureve5772@gmail.com>
2023-01-23 17:26:07 +0900

    유저목록 보이기

--- renderer/components/SideList.tsx
+++ renderer/components/SideList.tsx
@@ -1,27 +1,21 @@
 // 유저목록 보여주기
-import { useRouter } from "next/router";
-import { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 
-const SideList = () => {
-  const [friends, setFriends] = useState(["a", "b", "c"]);
-  const router = useRouter();
+interface SideListProps {
+  name: string;
+  image: string;
+}
 
+const SideList: React.FC<SideListProps> = ({ image, name }) => {
   return (
     <SideListContainer>
-      <div className="search-form">
-        <input type="text" placeholder="find user" />
-        <button>+</button>
-      </div>
-
-      {friends.map((friends) => (
-        <div className="user-chats">
-          <img src="/images/logo.png" />
-          <div className="user-info">
-            <span>지민</span>
-          </div>
+      <div className="user-chats">
+        <img src={image} />
+        <div className="user-info">
+          <span>{name}</span>
         </div>
-      ))}
+      </div>
     </SideListContainer>
   );
 };
@@ -30,19 +24,7 @@ export default SideList;
 
 const SideListContainer = styled.div`
   border-bottom: 1px solid gray;
-  .search-form {
-    display: flex;
-    padding: 10px;
-    input {
-      background-color: transparent;
-      border: none;
-      color: white;
-      outline: none;
-      ::placeholder {
-        color: lightgray;
-      }
-    }
-  }
+
   .user-chats {
     display: flex;
     padding: 10px;
